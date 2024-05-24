f4e3046b4417a021d372b78c0e6a955feb2fd751
nadirashvilininutsa <160115012+nadirashvilininutsa@users.noreply.github.com>
2024-05-24 12:51:53 +0400

    Add show action for users and display user's profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:edit, :update]
+  before_action :set_user, only: [:edit, :update, :show]
 
   def new
     @user = User.new
@@ -25,7 +25,11 @@ class UsersController < ApplicationController
     else
         render 'edit'
     end
-end
+  end
+
+  def show
+    @articles = @user.articles
+  end
 
   private
   def user_params
